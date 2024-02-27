81accc99d48bac4e5d5d1775faa6c2a9052f2f72
minyugyeong <alsdbrud123@naver.com>
2024-02-27 14:17:41 +0900

    두 수의 나눗셈

---
+++ src/main/java/programmers/Lv0/Solution_008.java
@@ -0,0 +1,8 @@
+package programmers.Lv0;
+
+public class Solution_008 {
+    public int solution(int num1, int num2) {
+        double answer = (double)num1 / num2 * 1000;
+        return (int)answer;
+    }
+}

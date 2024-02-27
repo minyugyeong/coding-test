e2200c273ec0400afb6f4794b35fdf2100ceb5cc
minyugyeong <alsdbrud123@naver.com>
2024-02-27 17:18:14 +0900

    아이스 아메리카노

---
+++ src/main/java/programmers/Lv0/Solution_018.java
@@ -0,0 +1,9 @@
+package programmers.Lv0;
+
+public class Solution_018 {
+    public int[] solution(int money) {
+        int[] answer = {money / 5500, money % 5500 };
+
+        return answer;
+    }
+}
